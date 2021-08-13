f449f965219a41f6d14dd53a5312b53b3cb8d3e5
Andrej-frontend <69960475+Andrej-frontend@users.noreply.github.com>
2021-08-13 13:31:53 +0200

    Add files via upload

---
+++ WincOpdrachten2021/Opdracht JS basics/index2.js
@@ -0,0 +1,12 @@
+
+let name = 'Mosh';  // String Literal
+let age = 30;  // Number Literal
+let isApproved = true;  // Boolean Literal
+let firstName = undefined; // Undefined
+let selectedColor = null; //Null:When clearing the value of an variable
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a numer (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive 
---
+++ WincOpdrachten2021/Opdracht JS basics/script.js
@@ -0,0 +1,22 @@
+// This is my first JavaScript code!
+console.log("Hello Winc Academy");
+let name = "Andrej";
+console.log(name);
+// Je kunt geen variabele aanmaken met de naam if omdat "if" een RESERVED KEYWORD is
+let sum = 4 + 4;
+console.log('sum:', sum);
+// "4" en "4" zijn STRING LITERALS, dus wiskundig niet optelbaar
+let sumWithStrings = "3" + "4";
+console.log("sumWithStrings:", sumWithStrings);
+let subtraction = 10 - 3;
+console.log("subtraction:", subtraction);
+let multiplication = 10 * 3;
+console.log("multiplication:", multiplication);
+let division = 12 / 3;
+console.log("division:", division);
+let modulus = 12 % 9;
+console.log("modulus:", modulus);
+let age = 48;
+console.log(typeof age);
+let ageAsString = "48";
+console.log(typeof ageAsString);
