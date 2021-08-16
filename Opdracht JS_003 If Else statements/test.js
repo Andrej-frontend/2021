0d3023c17cde302402064521683378ff29896ce4
Andrej-frontend <69960475+Andrej-frontend@users.noreply.github.com>
2021-08-16 21:09:23 +0200

    Add files via upload

---
+++ Opdracht JS_003 If Else statements/test.js
@@ -0,0 +1,29 @@
+
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob"
+
+/*
+Alternative data
+const age = 17;
+const isFemale = false;
+const driverStatus = "404"
+*/
+
+if (age >= 18) {
+    console.log("Je mag naar binnen, veel plezier!");
+} else {
+    console.log("U wordt vriendelijk verzocht buiten te blijven");
+}
+
+if (isFemale) {
+    console.log("Volgens de ticket ben je een vrouw!");
+} else {
+    console.log("Sorry: Tonight Females only");
+}
+
+if (driverStatus == "bob") {
+    console.log("Gefeliciteerd bob, u mag rijden!");
+} else {
+    console.log("Helaas, u bent geen bob en mag niet achter het stuur");
+}
---
+++ Opdracht JS_003 If Else statements/test1.js
@@ -0,0 +1,22 @@
+var youLikeMeat = true;
+var myNum = 8
+
+if (youLikeMeat) {
+
+    document.write("you like meat");
+
+} else {
+
+    document.write("you hate meat")
+}
+
+
+
+
+/*
+if (myNum == 10) {
+
+    document.write("myNum is equal to 10");
+
+} */
+
