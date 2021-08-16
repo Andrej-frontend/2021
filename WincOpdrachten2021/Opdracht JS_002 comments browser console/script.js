13150f91e67d9081d699c2732552877e54483429
Andrej-frontend <69960475+Andrej-frontend@users.noreply.github.com>
2021-08-16 21:08:25 +0200

    Add files via upload

---
+++ WincOpdrachten2021/Opdracht JS_002 comments browser console/script.js
@@ -0,0 +1,15 @@
+console.log("Hallo Winc Academy studenten")
+
+//Dit is een grote som
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+
+/* Hier een blok comment
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+*/
+
+// Met de comment kan je (een stukje) code uit/aan zetten in de console
+
+// Let op: runt JavaScript in NOde? Das is je terminal de console.
+// Runt JavaScript in je browser? Dan vind je de console in je developer tools.
