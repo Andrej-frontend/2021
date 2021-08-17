8ab6dcf1370dda932a22704424b87d84708e407d
Andrej-frontend <69960475+Andrej-frontend@users.noreply.github.com>
2021-08-17 21:03:10 +0200

    Add files via upload

---
+++ WincOpdrachten2021/Opdracht JS_005 Ref types_objects and arrays/part1.js
@@ -0,0 +1,22 @@
+
+// Objects
+const person = {
+    name: "Andrej",
+    age: 47,
+    evaluaties: [7, 10, 9]
+};
+
+// the full object
+console.log(person)
+
+// dot-notation
+console.log(person.name);
+console.log(person.age);
+
+//bracket-notation
+console.log(person["name"]);
+console.log(person["age"]);
+
+//key-value pair
+console.log(person.evaluaties);
+// de waarde van evaluaties is "ARRAY(3)";
---
+++ WincOpdrachten2021/Opdracht JS_005 Ref types_objects and arrays/part2.js
@@ -0,0 +1,19 @@
+//een lege array
+const legeArray = ["", "", ""];
+
+//gevuld met min 3 versch kleuren idv strings
+const myArray = ["green", "blue", "red"];
+
+//console.log(myArray);
+console.log(myArray.length);
+
+//1e element vd array
+console.log(myArray[0]);
+
+/* laatste element uit onbekende array lengte:
+var last_element = myArray[myArray.length - 1]
+console.log(last_element)
+*/
+
+//kortere versie
+console.log(myArray[myArray.length - 1]);
