6674c8ef1b6954e99b84fb1ea1a13e73b107caee
Andrej-frontend <69960475+Andrej-frontend@users.noreply.github.com>
2021-08-18 13:11:52 +0200

    Add files via upload

--- WincOpdrachten2021/Opdracht JS_005 Ref types_objects and arrays/part2.js
+++ WincOpdrachten2021/Opdracht JS_005 Ref types_objects and arrays/part2.js
@@ -1,10 +1,7 @@
-//een lege array
-const legeArray = ["", "", ""];
 
 //gevuld met min 3 versch kleuren idv strings
 const myArray = ["green", "blue", "red"];
-
-//console.log(myArray);
+console.log(myArray);
 console.log(myArray.length);
 
 //1e element vd array
@@ -14,6 +11,18 @@ console.log(myArray[0]);
 var last_element = myArray[myArray.length - 1]
 console.log(last_element)
 */
-
 //kortere versie
 console.log(myArray[myArray.length - 1]);
+
+//adding elements
+myArray.push("yellow");
+console.log(myArray);
+myArray.push(5);
+console.log(myArray);
+
+//adding an object
+myArray.push({ greeting: "Hi ik ben een object" });
+console.log(myArray);
+
+//last greeting log
+console.log(myArray[myArray.length - 1].greeting);
